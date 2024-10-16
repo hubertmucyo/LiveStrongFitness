a68f6a561b1070d62082dee7674a6d5fa9e594b7
hubertmucyo <h.mucyo@alustudent.com>
2024-10-16 21:20:24 +0530

    like git

--- send_email.php
+++ send_email.php
@@ -1,42 +1,49 @@
 <?php
 
+// Enable error reporting
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+// Import PHPMailer classes into the global namespace
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require 'path/to/PHPMailer/src/Exception.php';
-require 'path/to/PHPMailer/src/PHPMailer.php';
-require 'path/to/PHPMailer/src/SMTP.php';
+// Load Composer's autoloader
+require 'vendor/autoload.php';
 
+// Check if the form was submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $website = $_POST['website'];
     $comment = $_POST['comment'];
 
+    // Create an instance; passing `true` enables exceptions
     $mail = new PHPMailer(true);
 
     try {
-        //Server settings
-        $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';
-        $mail->SMTPAuth = true;
-        $mail->Username = 'marshaquavo@gmail.com'; // Your Gmail address
-        $mail->Password = 'qwerasdzx:123'; // Your Password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port = 587;
+        // Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                // Enable verbose debug output
+        $mail->isSMTP();                                      // Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                 // Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                             // Enable SMTP authentication
+        $mail->Username   = 'marshaquavo@gmail.com';          // SMTP username
+        $mail->Password   = 'your_app_password_here';         // SMTP password (use an App Password for Gmail)
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;      // Enable implicit TLS encryption
+        $mail->Port       = 465;                              // TCP port to connect to, use 587 if using ENCRYPTION_STARTTLS
 
-        //Recipients
+        // Recipients
         $mail->setFrom('marshaquavo@gmail.com', 'Hubert');
-        $mail->addAddress('marshaquavo@gmail.com'); // Add your email address
+        $mail->addAddress('marshaquavo@gmail.com');           // Add your email address
 
         // Content
-        $mail->isHTML(true);
+        $mail->isHTML(true);                                  // Set email format to HTML
         $mail->Subject = 'New Comment Submission';
-        $mail->Body = "<strong>Name:</strong> $name<br><strong>Email:</strong> $email<br><strong>Website:</strong> $website<br><strong>Comment:</strong> $comment";
+        $mail->Body    = "<strong>Name:</strong> $name<br><strong>Email:</strong> $email<br><strong>Website:</strong> $website<br><strong>Comment:</strong> $comment";
+        $mail->AltBody = "Name: $name\nEmail: $email\nWebsite: $website\nComment: $comment"; // Plain text for non-HTML clients
 
+        // Send the email
         $mail->send();
         echo 'Message has been sent';
     } catch (Exception $e) {
